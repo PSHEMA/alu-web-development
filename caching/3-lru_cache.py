806a86a8dc50febd9c9974c9eb2f875f8c268e9a
PSHEMA <p.shema@alustudent.com>
2024-09-12 14:04:11 +0200

    LRU cashing

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-""" LRU Caching Module
 """
-
+LRUCache class that inherits from BaseCaching and is a caching system
+"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRU Caching Class
-    """
+    """ Define LRUCache """
+
     def __init__(self):
-        """ Initiliaze
-        """
+        """ Initialize LRUCache """
+        self.queue = []
         super().__init__()
-        self.lru_keys = []
 
     def put(self, key, item):
-        """ Add an item in the cache
         """
-        if key is None or item is None:
-            return
-        
-        if key in self.cache_data:
-            self.lru_keys.remove(key)
-
-        self.cache_data[key] = item
-        self.lru_keys.append(key)
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            lru_key = self.lru_keys.pop(0)
-            print("DISCARD: {}".format(lru_key))
-            del self.cache_data[lru_key]
+        Assign the item to the dictionary
+        """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key is None or key not in self.cache_data:
-            return None
-
+        """ Return the value associated with the given key """
+        if self.cache_data.get(key):
+            self.queue.remove(key)
+            self.queue.append(key)
         return self.cache_data.get(key)
