edd69a2f2b79703b2ad36e8dde1956d01a78aa24
PSHEMA <p.shema@alustudent.com>
2024-09-12 13:06:21 +0200

    Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -13,13 +13,11 @@ class BasicCache(BaseCaching):
 
         if key is None or item is None:
             return
-
         self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
-
         if key is None or key not in self.cache_data:
             return None
-
         return self.cache_data.get(key)
+    
