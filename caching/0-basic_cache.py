efe0726ccd77d0791a762028bb0e7c4c47fe91e6
PSHEMA <p.shema@alustudent.com>
2024-09-12 13:07:58 +0200

    Basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" Basic dictionary """
+"""
+BasicCache class that inherits from BaseCaching
+"""
 
-
-BaseCaching = __import__('base_Cashing').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Basic dictionary caching module """
+    """ Define BasicCache """
 
     def put(self, key, item):
-        """ Add an item in the cache """
-
+        """ Add key/value pair to the cache """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
+        """ Retrieve value from the cache based on given key """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
-    
