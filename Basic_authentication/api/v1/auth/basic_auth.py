17ac13907cfb96741af725782429fa46fb50f131
PSHEMA <p.shema@alustudent.com>
2024-09-18 12:05:36 +0200

    Basic - Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Auth Class
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -17,3 +18,15 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """ decode_base64_authorization_header
+        """
+        if not base64_authorization_header or type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
+
