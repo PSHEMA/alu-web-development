aa8e8ad76c34873cf07227074672b061b04d1bcd
PSHEMA <p.shema@alustudent.com>
2024-09-18 12:20:15 +0200

    Basic - User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,15 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ extract_user_credentials
+        """
+        if not decoded_base64_authorization_header:
+            return None, None
+        if type(decoded_base64_authorization_header) != str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        return decoded_base64_authorization_header.split(':', 1)
