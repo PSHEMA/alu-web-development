4ba79fa36bf0950ad6c7e24c2f913226e5e60737
PSHEMA <p.shema@alustudent.com>
2024-09-18 18:58:20 +0200

    Empty session

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Session Authentication
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Session Authentication
+    """
