f7ddac0c8b295fd8a28ba0c7cc49551192660b9c
PSHEMA <p.shema@alustudent.com>
2024-09-18 18:48:58 +0200

    Et moi et moi et moi!

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -63,7 +63,7 @@ def before_request() -> str:
     if auth.current_user(request) is None:
         abort(403)
 
-    auth.correctly_request(request) = request.current_user
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
