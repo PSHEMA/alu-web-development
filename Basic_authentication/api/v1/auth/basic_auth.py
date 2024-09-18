8f1c3e386c323f21565e67fb9738d8322ff9cacb
PSHEMA <p.shema@alustudent.com>
2024-09-18 12:26:14 +0200

    Basic - User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ Auth Class
 """
 import base64
+from typing import TypeVar
+from Basic_authentication.models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -45,3 +47,23 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return decoded_base64_authorization_header.split(':', 1)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str
+                                     ) -> TypeVar('User'):
+        """ user_object_from_credentials
+        """
+        if not user_email or type(user_email) != str:
+            return None
+        if not user_pwd or type(user_pwd) != str:
+            return None
+        try:
+            user = User.search({'email': user_email})
+        except Exception:
+            return None
+        if not user:
+            return None
+        if user[0].is_valid_password(user_pwd):
+            return user[0]
+        return None
