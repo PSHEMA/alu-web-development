ecde4dacce6aa30aa8c099ecc2882d57e91adc78
PSHEMA <p.shema@alustudent.com>
2024-09-24 19:52:22 +0200

    Create User

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -7,25 +7,24 @@ from sqlalchemy.orm import sessionmaker
 from user import Base, User
 
 class DB:
-    """Class to manage the MySQL database."""
+    """ DB class """
+
     def __init__(self):
-        """Initialize a new DB instance."""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Private session property to create the session when needed."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a user to the database with the provided email and hashed_password."""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """ add user method """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
