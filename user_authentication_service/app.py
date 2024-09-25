0b0666a7eb9d0e64aedcb898ec8034a6f2237360
PSHEMA <p.shema@alustudent.com>
2024-09-25 19:54:08 +0200

    Register User

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -4,8 +4,24 @@
 
 from flask import Flask
 import flask
+from auth import Auth
 
 app = Flask(__name__)
+auth = Auth()
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+    Register a new user
+    """
+    email = flask.request.form.get("email")
+    password = flask.request.form.get("password")
+    try:
+        user = auth.register_user(email, password)
+        return flask.jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return flask.jsonify({"message": "email already registered"}), 400
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
