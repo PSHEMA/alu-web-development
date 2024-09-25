d9ddda28027a29bb4ecc9f6daa552365bd10831e
PSHEMA <p.shema@alustudent.com>
2024-09-25 18:50:00 +0200

    Register User

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """ Auth
 """
 import bcrypt
+from db import DB
 from user import User
 
 
@@ -9,3 +10,20 @@ def _hash_password(password: str) -> str:
     """ Hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialize a new Auth instance.
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user
+        """
+        if self._db.find_user_by(email=email) is not None:
+            raise ValueError("User {email} already exists")
+        new_user = self._db.add_user(email, _hash_password(password))
+        return new_user
