35da11d71470032b7d3ac17ec4974d0ee3b618b3
PSHEMA <p.shema@alustudent.com>
2024-09-26 19:40:52 +0200

    Generate reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -77,3 +77,14 @@ class Auth:
             return None
         else:
             self._db.update_user(user.id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ returns a string or None """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
