ad108fa79b45b6f3ac2b1e0dd28881a36b7bbc19
PSHEMA <p.shema@alustudent.com>
2024-09-26 18:47:43 +0200

    Login

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -41,20 +41,13 @@ def login() -> str:
     """
     email = flask.request.form.get("email")
     password = flask.request.form.get("password")
-    if email is None or email == "":
-        return flask.jsonify({"error": "email missing"}), 400
-    if password is None or password == "":
-        return flask.jsonify({"error": "password missing"}), 400
-    try:
-        user = auth.valid_login(email, password)
-        if user is False:
-            return flask.jsonify({"error": "no user found for this email"}), 404
-        session_id = auth.create_session(user.id)
-        return flask.jsonify({"email": user.email,
-                              "message": "logged in",
-                              "session_id": session_id}), 200
-    except ValueError:
-        return flask.jsonify({"error": "wrong password"}), 401
+    if Auth.valid_login(email, password):
+        session_id = auth.create_session(email)
+        resp = flask.jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
+    else:
+        flask.abort(401)
 
 
 if __name__ == "__main__":
