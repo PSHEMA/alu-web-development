b1f95150865e878dbe340af7ff14a2f2f4c867d7
PSHEMA <p.shema@alustudent.com>
2024-09-26 19:26:01 +0200

    Destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -67,3 +67,13 @@ class Auth:
             return None
         else:
             return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroys a session
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            self._db.update_user(user.id, session_id=None)
