b32a029baead68c8ab3d29515676a381eae1caa4
PSHEMA <p.shema@alustudent.com>
2024-09-26 19:22:35 +0200

    Find User by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -59,13 +59,11 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> str | None:
-        """Get user from session id
-        """
-        if session_id is None:
-            return None
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ returns a string or None """
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user
         except NoResultFound:
             return None
+        else:
+            return user
