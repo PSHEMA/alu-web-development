d0cd46133936f61cf8c5572db6260ecaf9382ca9
PSHEMA <p.shema@alustudent.com>
2024-09-26 18:45:39 +0200

    Login

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -34,5 +34,28 @@ def index() -> str:
     return flask.jsonify({"message": "Bienvenue"})
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions
+    Login
+    """
+    email = flask.request.form.get("email")
+    password = flask.request.form.get("password")
+    if email is None or email == "":
+        return flask.jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return flask.jsonify({"error": "password missing"}), 400
+    try:
+        user = auth.valid_login(email, password)
+        if user is False:
+            return flask.jsonify({"error": "no user found for this email"}), 404
+        session_id = auth.create_session(user.id)
+        return flask.jsonify({"email": user.email,
+                              "message": "logged in",
+                              "session_id": session_id}), 200
+    except ValueError:
+        return flask.jsonify({"error": "wrong password"}), 401
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
